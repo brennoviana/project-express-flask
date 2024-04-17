33ada16c82120fd0f65c177948a0e29d0614d07d
brenno.viana <brennovianacoelho@gmail.com>
2024-04-16 22:06:26 -0300

    sla2

--- flask-project/app.py
+++ flask-project/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__, template_folder='app/templates')
 
 @app.route('/movies')
-def hello():
+def login():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000, debug=True)

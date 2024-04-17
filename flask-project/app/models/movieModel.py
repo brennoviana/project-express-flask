5eeacc4c81015d21e1b18bb02e9b3aae1e49c4a1
brenno.viana <brennovianacoelho@gmail.com>
2024-04-17 09:37:40 -0300

    sla2

--- flask-project/app/models/movieModel.py
+++ flask-project/app/models/movieModel.py
@@ -4,4 +4,4 @@ db = SQLAlchemy()
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    namae = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
--- flask-project/app/routes/movieRoutes.py
+++ flask-project/app/routes/movieRoutes.py
@@ -1,7 +1,17 @@
 from flask import Blueprint, render_template
+from app import db
+from sqlalchemy import text
 
 movies = Blueprint('movies', __name__)
 
 @movies.route('/')
 def home():
     return render_template('index.html')
+
+@movies.route('/test_db')
+def test_db():
+    try:
+        db.session.execute(text('SELECT 1'))
+        return "Conexão com o banco de dados está funcionando!"
+    except Exception as e:
+        return f"Erro ao conectar com o banco de dados: {e}"
